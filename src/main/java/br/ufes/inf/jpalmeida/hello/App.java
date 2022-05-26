32924c978e367f590b5be99a8a87107e733e63cc
João Paulo A. Almeida <jpandradealmeida@gmail.com>
2022-05-26 14:16:34 -0300

    Adicionado figlet

--- src/main/java/br/ufes/inf/jpalmeida/hello/App.java
+++ src/main/java/br/ufes/inf/jpalmeida/hello/App.java
@@ -1,13 +1,19 @@
 package br.ufes.inf.jpalmeida.hello;
 
+import java.io.IOException;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+     // using default font standard.flf, obtained from maven artifact
+     String asciiArt1 = FigletFont.convertOneLine(args[0]);
+     System.out.println(asciiArt1);
     }
 }
